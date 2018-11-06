4feb194e4aab54bf381d3a9696ef13c3d76ddb3d
Kim Seungha <seungha.me@gmail.com>
2018-11-06 11:22:59 +0900

    로그인 여부에 따라 다른 페이지가 나오도록 수정

--- src/index.js
+++ src/index.js
@@ -102,4 +102,11 @@ async function drawTodoList() {
   rootEl.appendChild(fragment)
 }
 
-drawLoginForm()
+// 만약 로그인을 한 상태라면 바로 할 일 목록을 보여주고
+if (localStorage.getItem('token')) {
+  drawTodoList()
+} else {
+  // 아니라면 로그인 폼을 보여준다.
+  drawLoginForm()
+}
+
